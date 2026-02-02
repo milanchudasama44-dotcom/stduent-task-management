11ab22f33beccddfe8fa7da59df114120e67f5b5
unknown <you@example.com>
2026-02-02 12:07:03 +0530

    [AA] [file uploded]

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
